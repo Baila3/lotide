61b59090d9d935bd63564f5d077ce82805c9ba19
Baila3 <bailaly16@gmail.com>
2022-04-05 12:49:30 -0400

    tail array

---
+++ tail.js
@@ -0,0 +1,8 @@
+
+const assertEqual = function(Arr) {
+return Arr.slice(-1)[0] 
+  };
+  
+  // TEST CODE
+  console.log(assertEqual(["Lighthouse Labs", "Bootcamp"]));
+  console.log(assertEqual([1, 2]));

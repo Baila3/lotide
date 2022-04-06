08d4cf9493c6e812fe06698fb4bed68bdc9a0baa
Baila3 <bailaly16@gmail.com>
2022-04-06 17:01:40 -0400

    find letter pos

---
+++ letterPositions.js
@@ -0,0 +1,46 @@
+const eqArrays = function(arr1, arr2){
+  if(arr1.length !== arr2.length){
+    return false
+  } 
+   for (let i = 0; i < arr1.length; i++){
+   if (arr1[i] !== arr2[i]){
+     return false
+   } 
+ 
+  }
+   return true
+  };
+
+  const assertArraysEqual = function(ray1, ray2){
+  if(ray1.length !== ray2.length){
+    return false
+  } 
+   for (let i = 0; i < ray1.length; i++){
+   if (ray1[i] !== ray2[i]){
+     return "Assertion Failed: These don't look the same";
+   } 
+ 
+  }
+   return "Assertion Passed: Like identical twins";
+  };
+
+  const letterPositions = function(sentence) {
+    let results = {};
+  for(let i = 0; i < sentence.length; i++){
+      // if (results[i] !== sentence){
+      //     results[sentence[i]] = []; 
+      //     results[sentence[i]].push(i)
+      //  }
+      if(results[sentence[i]] && sentence[i] !== " ") {
+  
+        // results = {...results, [sentence[i]] : [...results[sentence[i]], i]}
+        results[sentence[i]] = [results[sentence[i]], i]
+      } else {
+        // results = {...results, [sentence[i]] : [i]}
+        results[sentence[i]] = i
+      }
+  }
+  return results;
+  };
+
+  console.log(letterPositions("salmonno"));

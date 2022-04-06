1621df5c3368e4bd64de42d87e161f4a19121b0e
Baila3 <bailaly16@gmail.com>
2022-04-06 10:10:51 -0400

    remove element

--- eqArrays.js
+++ eqArrays.js
@@ -10,7 +10,7 @@ const assertEqual = function(actual, expected) {
 const eqArrays = function(arr1, arr2){
  if(arr1.length !== arr2.length){
    return false
- } else {
+ } 
   for (let i = 0; i < arr1.length; i++){
   if (arr1[i] !== arr2[i]){
     return false
@@ -18,7 +18,7 @@ const eqArrays = function(arr1, arr2){
 
   }
   return true
- }
+
  };
 
   

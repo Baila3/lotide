7c75365ee97759d0bde77d074392dc2d77cee6a1
Baila3 <bailaly16@gmail.com>
2022-04-05 21:23:09 -0400

    assert

---
+++ eqArrays.js
@@ -0,0 +1,29 @@
+
+const assertEqual = function(actual, expected) {
+  if(actual === expected){
+    return `Assertion Passed:  ${actual}  === ${expected}`;
+  } else {
+    return `Assertion Failed: ${actual}  !== ${expected}`;
+  }
+  };
+  
+const eqArrays = function(arr1, arr2){
+ if(arr1.length !== arr2.length){
+   return false
+ } else {
+  for (let i = 0; i < arr1.length; i++){
+  if (arr1[i] !== arr2[i]){
+    return false
+  } 
+
+  }
+  return true
+ }
+ };
+
+  
+  // TEST CODE
+  console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3])));
+  console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+
+  

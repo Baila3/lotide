035c55add58f83ee724425abc58f6334a6b49ad0
Baila3 <bailaly16@gmail.com>
2022-04-11 15:04:54 -0400

    new exports head 2

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,3 @@
-// const assertEqual = require('../assertEqual');
-// const head = require('../head');
-// console.log(assertEqual(head(["Lighthouse Labs", "Bootcamp"]),"Lighthouse Labs" ));
-// console.log(assertEqual(head([1, 1]), 1));
 
 const assert = require('chai').assert;
 const head   = require('../head');

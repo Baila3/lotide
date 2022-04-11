21dd6bd66f0cb766000095543a5fe7d4a116ceee
Baila3 <bailaly16@gmail.com>
2022-04-11 15:53:17 -0400

    slight changes

--- middle.js
+++ middle.js
@@ -4,10 +4,9 @@
     if (array.length % 2 === 0 || array.length === 1){
       return []
     } else{
-      arr.push((array.length/2) + .5)
-      return arr
+     return (array.length/2) + .5
     }
     
     }
-    console.log(middle([1,2,3,4,5]))
+
   module.exports = middle;

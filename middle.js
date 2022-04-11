f2fa6f13a21ed70ecc628b7ca480a1923dc2fa89
Baila3 <bailaly16@gmail.com>
2022-04-11 14:47:21 -0400

    middle exports

--- middle.js
+++ middle.js
@@ -1,30 +1,13 @@
-const eqArrays = function(arr1, arr2){
-  if(arr1.length !== arr2.length){
-    return false
-  } 
-   for (let i = 0; i < arr1.length; i++){
-   if (arr1[i] !== arr2[i]){
-     return false
-   } 
- 
-   }
-   return true
- 
-  };
-  const assertEqual = function(actual, expected) {
-    if(actual === expected){
-      return `✅✅✅ Assertion Passed:  ${actual}  === ${expected}`;
-    } else {
-      return `🛑🛑🛑 Assertion Failed: ${actual}  !== ${expected}`;
-    }
-    };
 
     const middle = function(array) {
+    arr = []
     if (array.length % 2 === 0 || array.length === 1){
       return []
     } else{
-      return (array.length/2) + .5
+      arr.push((array.length/2) + .5)
+      return arr
     }
     
     }
     console.log(middle([1,2,3,4,5]))
+  module.exports = middle;

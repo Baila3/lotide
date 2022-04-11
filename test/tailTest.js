0faffa327ee70175fb709db8397a39529a2f07b9
Baila3 <bailaly16@gmail.com>
2022-04-11 15:16:47 -0400

    new import tail

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,4 +1,12 @@
+
+const assert = require('chai').assert;
+const tail  = require('../tail');
 const assertEqual = require('../assertEqual');
-const tail = require('../tail');
-console.log(assertEqual(tail(["Lighthouse Labs", "Bootcamp"]),"Bootcamp"));
-console.log(assertEqual(tail([1, 2]), 2));
+describe("#tail", () => {
+  it("returns 3 for [1, 2, 3]", () => {
+    assert.strictEqual(tail([1, 2, 3]), 3);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(tail(['5']), '5'); 
+  });
+});

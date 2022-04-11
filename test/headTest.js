dd7e91c0a38a7d0a232c0011596d0a489b013b5c
Baila3 <bailaly16@gmail.com>
2022-04-11 14:59:50 -0400

    new import head

--- test/headTest.js
+++ test/headTest.js
@@ -1,4 +1,16 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
-console.log(assertEqual(head(["Lighthouse Labs", "Bootcamp"]),"Lighthouse Labs" ));
-console.log(assertEqual(head([1, 1]), 1));
+// const assertEqual = require('../assertEqual');
+// const head = require('../head');
+// console.log(assertEqual(head(["Lighthouse Labs", "Bootcamp"]),"Lighthouse Labs" ));
+// console.log(assertEqual(head([1, 1]), 1));
+
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+});
